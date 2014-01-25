81ff51af805561825f61774fdfa18f7dc656c228
dmitri <dmitri@localhost.(none)>
2014-01-24 20:38:32 -0500

    Add location example.

--- example-map/scavengehunt/scavenge.js
+++ example-map/scavengehunt/scavenge.js
@@ -165,3 +165,32 @@ if (Meteor.isServer) {
 	}
     );
 }
+
+if (Meteor.isClient) {
+
+    Meteor.setInterval(function() {
+
+	var options = {
+	    enableHighAccuracy: true,
+	    timeout: 5000,
+	    maximumAge: 0
+	};
+
+	var success = function(pos) {
+	    var crd = pos.coords;
+
+	    console.log('Your current position is:');
+	    console.log('Latitude : ' + crd.latitude);
+	    console.log('Longitude: ' + crd.longitude);
+	    console.log('More or less ' + crd.accuracy + ' meters.');
+	};
+	
+	var error = function(err) {
+	    console.warn('ERROR(' + err.code + '): ' + err.message);
+	};
+	
+	navigator.geolocation.getCurrentPosition(success, error, options);
+
+    }, 5000);
+
+}
